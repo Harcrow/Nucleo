a511f55aeb0901f93c67bc96c3afe8da609087f4
Tyler Jowaisas <tjowaisas@evolvtechnology.com>
2022-08-26 22:05:10 -0400

    Started a driver.  Made Digital Output function.  Started Digital IN function

--- Source/src/GPIO.c
+++ Source/src/GPIO.c
@@ -1,34 +1,54 @@
 #include "GPIO.h"
 
+//MODER settings
 #define INPUT 0
-#define GENERAL_PURPOSE 1
-#define ALTERNATE 2
-#define ANALOG 3
-#define PUSH_PULL 0
+#define OUTPUT 1
+
+#define PUSH_PULL 0 
 #define OPEN_DRAIN 1
 
-int pinMode(PORT, PIN, MODE){
 
+
+int setDigitalOutput(PORT, PIN, MODE){
+
+	//RCC enable of APB2ENR register 
 	RCC -> APB2ENR |= RCC_APB2ENR_SYSCFGEN;
-	if(port == GPIOA){
+	
+	if(PORT == PORT_A){
+		//enable RCC clock for PORT A
 		RCC -> AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+		
+		//Resetting the two MODER bits (generally doesn't have to happen with single bit registers)
+		GPIOA -> MODER &= (INPUT << PIN*2);
+	
+		GPIOA -> MODER |= (OUTPUT << PIN*2);
+		GPIOA -> OTYPER &= (MODE << PIN);
+		
 	}
-	if(port == GPIOB){
+	if(port == PORT_B){
 		RCC -> AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
+		//enable RCC clock for PORT A
+		
+		GPIOB -> MODER &= (INPUT << PIN*2);
+		
+		GPIOB -> MODER |= (OUTPUT << PIN*2);
+		GPIOB -> OTYPER &= (MODE << PIN);
+		
 	}
-	if(port == GPIOC){
+	if(port == PORT_C){
 		RCC -> AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
+		
+		GPIOC -> MODER &= (INPUT << PIN*2);
+		
+		GPIOC -> MODER |= (OUTPUT << PIN*2);
+		GPIOC -> OTYPER &= (MODE << PIN);
+		
 	}
 	
-	//RCC enable of APB2ENR register 
+int setDigitalInput(PORT, PIN, 
 	
 	
 	//RCC == reset and clock control
-	
-	
-	
-	
-	
 	
 	//setting output pushpull mode of PA5
 	
